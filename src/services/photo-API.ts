11e0092b45d6ce99bb5b86b4e2387bd71cb1bdeb
Arthur Procópio <avprocopio@gmail.com>
2023-07-11 14:13:58 -0300

    add error handling to request

--- src/services/photo-API.ts
+++ src/services/photo-API.ts
@@ -7,6 +7,9 @@ export const fetchPhotos = async (searchTerm: string): Promise<Photo[]> => {
     headers: { Authorization: `Client-ID ${unspashToken}` },
   };
   const response = await fetch(`https://api.unsplash.com/search/photos?query=${searchTerm}`, header);
-  const data = await response.json();
-  return data.results;
+  if (response.ok) {
+    const data = await response.json();
+    return data.results;
+  }
+  throw new Error('Request failed!');
 };
