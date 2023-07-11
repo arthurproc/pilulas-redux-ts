bffdc9094aaf954d77d7598293ad9077e1885b7d
Arthur Procópio <avprocopio@gmail.com>
2023-07-11 14:14:40 -0300

    remove rule for eslint and add null assertion on main

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 import './index.css';
 import store from './redux';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Provider store={ store }>
       <App />
